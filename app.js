92258cd15dee69e4f08fe21e8eecdb297ea84f80
João Martins <j.martins@NORDWARE.IO>
2021-04-14 12:23:17 -0300

    updating packagejson and app

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const PORT = proccess.env.PORT || 3000;
 
-app = express();
+appServer = express();
 
-app.get('/', (req, res) => {
+appServer.get('/', (req, res) => {
     res.send('Hello Friend!');
 });
 
-app.listen(PORT, () => {
+appServer.listen(PORT, () => {
     console.log(`App is running on port: ${PORT}`)
 });
 

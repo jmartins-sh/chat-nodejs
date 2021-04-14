a117f365748ac2f4a92a0efd8d648c2ea1deec2a
João Martins <j.martins@NORDWARE.IO>
2021-04-14 12:42:38 -0300

    new things

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+const app = express();
+const webServer = require('http').createServer(app);
+const io = require('socket.io')(webServer);
 
-appServer = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-appServer.get('/', (req, res) => {
-    res.send('Hello Friend!');
+app.get('/', (req, res) => {
+    res.render('index.html');
 });
 
-appServer.listen(PORT, () => {
+webServer.listen(PORT, () => {
     console.log(`App is running on port: ${PORT}`)
 });
